0651bbee920d027633d21d85572227dc6848d131
fallen.ink <fengzilijie@gmail.com>
2015-12-15 13:46:44 +0800

    添加扩展文件，注意文件的命名规则

---
+++ String+/String.size.swift
@@ -0,0 +1,34 @@
+//
+//  String.size.swift
+//  SwiftProject
+//
+//  Created by fallen.ink on 12/15/15.
+//  Copyright © 2015 王涛. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    /**
+     *
+     */
+    public func textSizeWith(fontSize:CGFloat,width:CGFloat)->CGSize{
+        let font = UIFont.systemFontOfSize(fontSize)
+        let size = CGSizeMake(width, CGFloat.max)
+        let text = self as NSString
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = .ByWordWrapping;
+        let attributes = [NSFontAttributeName:font,NSParagraphStyleAttributeName:paragraphStyle.copy()]
+        let rect = text.boundingRectWithSize(size, options: .UsesLineFragmentOrigin, attributes: attributes, context: nil)
+        return rect.size
+        
+    }
+    
+    /**
+     *
+     */
+    public func textSizeForOneLineWith(fontSize:CGFloat)->CGSize {
+        return self.textSizeWith(fontSize, width: CGFloat.max)
+    }
+}
