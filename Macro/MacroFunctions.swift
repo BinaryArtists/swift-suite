766ecc24c73282d5a111b492d03d9971758fefaa
王云鹏 <571533442@qq.com>
2016-03-17 17:46:59 +0800

    ss

--- Macro/MacroFunctions.swift
+++ Macro/MacroFunctions.swift
@@ -31,12 +31,39 @@ func DBNib(nibName:String) ->UINib{
 let DBWidth:CGFloat = UIScreen.mainScreen().bounds.size.width
 let DBHeight:CGFloat = UIScreen.mainScreen().bounds.size.height
 
-/**
-*  简写的实例化
-*/
+
+//MARK:简写的实例化
 let DBDefault = NSUserDefaults.standardUserDefaults()
 let DBNotificationCenter = NSNotificationCenter.defaultCenter()
 let DBAppDelegate  = UIApplication.sharedApplication().delegate as! AppDelegate
 
 let DBTrue = true
 let DBFalse = false
+
+
+//MARK:流水线加工控件
+/**
+流水线label
+居中 背景白色label
+- returns: 居中 背景白色label 12号字体 灰度102，102，102  单行
+*/
+func factoryLabel() -> UILabel{
+    let label = UILabel()
+    label.textAlignment = .Center
+    label.backgroundColor = UIColor.whiteColor()
+    label.font = UIFont.systemFontOfSize(12)
+    label.numberOfLines = 1
+    label.textColor = DBRgb(102, g: 102, b: 102)
+    return label
+}
+
+/**
+ 流水线View
+ 背景白色
+ */
+func factoryView() -> UIView{
+    let view = UIView()
+    view.backgroundColor = UIColor.whiteColor()
+    return view
+}
+
