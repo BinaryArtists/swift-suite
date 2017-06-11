9b9a8ecfe42a96e3742c9a901c8c3ac2aa120fd2
fallen.ink <fengzilijie@qq.com>
2017-06-11 13:24:27 +0800

    +

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame:UIScreen.main.bounds)
         
-        window?.rootViewController = UIViewController()
+        window?.rootViewController = UserListViewController()
         
         window?.makeKeyAndVisible()
         
