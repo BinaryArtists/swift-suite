f7310077daae4687092ea4d657c96b6251b87430
fallen.ink <fengzilijie@qq.com>
2017-07-02 11:48:38 +0800

    +

---
+++ _Building/Appearance/Appearance.swift
@@ -0,0 +1,12 @@
+//
+//  Appearance.swift
+//  readmate
+//
+//  Created by fallen.ink on 01/07/2017.
+//  Copyright © 2017 fallen.ink. All rights reserved.
+//
+
+import Foundation
+
+let color_background_view = color_with_rgb(red: 248, green: 248, blue: 248)
+let color_navigation_bar = color_with_rgb(red: 248, green: 248, blue: 248)
---
+++ _Building/Base/_Interactor.swift
@@ -0,0 +1,22 @@
+//
+//  BaseInteractor.swift
+//  readmate
+//
+//  Created by fallen.ink on 02/07/2017.
+//  Copyright © 2017 fallen.ink. All rights reserved.
+//
+
+import Foundation
+
+/**
+    @knowledge
+ 
+    - 接收 ViewController's Action
+    - 调用 DAO
+    - 调用 Presentor
+    - 调用 Router
+ */
+class _Interactor {
+    
+    // Hold Presentor
+}
